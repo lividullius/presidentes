08c130ea61f41aa3795ba89820d0cfc8ea531a9f
Danckwardtt <sophia.danckwardt@edu.pucrs.br>
2024-09-04 17:12:30 -0300

    Update Presidentes.java
    Comentários e explicação da classe

--- Presidentes.java
+++ Presidentes.java
@@ -1,11 +1,25 @@
+/**
+ * A classe Presidentes representa dados sobre um presidente da história.
+ * 
+ * Cada presidentes é representado por sete campos de dados.
+ * Os dados foram escolhidos com base na lista de presidentes brasileiros / estado-unidenses, dados 
+ * encontrados na wikipedia.
+ * @see https://pt.wikipedia.org/wiki/Lista_de_presidentes_do_Brasil
+ * @see https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States
+ * @author sophia.danckwardt@edu.pucrs.br
+ * @author @edu.pucrs.br
+ * @author @edu.pucrs.br
+ */
+
 public class Presidentes{
-private int numero;
-    private String nome;
-    private String nomeVice;
-    private String partido;
-    private int anoinicio;
-    private int anofim;
-    private String posse;
+    private int numero; //número em ordem de mandato
+    private String nome; //nome do presidente
+    private String nomeVice; // nome do vice presidente
+    private String partido; //partido do qual pertencia o presidente
+    private int anoinicio; //ano inicial do seu mandato
+    private int anofim; //ano final do seu mandato
+    private String posse; // como chegou ao poder: Golpe de Estado, Renuncia do antecessor, morte do antecessor, eleito diretamente ou indiretamente
+    
     public Presidentes(int numero,String nome, String nomeVice,String partido, int anoinicio, int anofim, String posse){
         this.numero=numero;
         this.nome=nome;
@@ -15,6 +29,7 @@ private int numero;
         this.anofim=anofim;
         this.posse=posse;
     }
+    //metodos getters
     public int getNumero(){
         return numero;
     }
@@ -36,6 +51,7 @@ private int numero;
     public String getPosse(){
         return posse;
     }
+    //metodos setters
     public void setNumero(int novoNumero){
         numero=novoNumero;
     }
@@ -57,7 +73,8 @@ private int numero;
     public void setPosse(String novaPosse){
         posse=novaPosse;
     }
-    public String toString(){
+    public String toString(){ 
+        //retorna os dados coletados em formato de texto
         return "\n Presidente Número: " +numero+ "\n Nome do Presidente: " +nome+ "\n Nome do Vice-Presidente: " +nomeVice+ "\n Partido: " +partido+
         "\n Ano de inicio do mandato: " +anoinicio+ "\n Ano de fim de mandato: " +anofim+
         "\n Meio pelo qual assumiu a presidência: " +posse;
